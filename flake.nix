3c667d55ca952ff0e16830cc31804c6f4dbc562c
Lin Yinfeng <lin.yinfeng@outlook.com>
2025-04-29 13:24:32 +0800

    Remove rustfmt from treefmt configuration

--- flake.nix
+++ flake.nix
@@ -142,7 +142,6 @@
               projectRootFile = "flake.nix";
               programs = {
                 nixfmt.enable = true;
-                rustfmt.enable = true;
                 shfmt.enable = true;
               };
             };
